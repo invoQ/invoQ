8f5102676319badf699cff3c8c527a2d58478496
invoQ <invoQs@gmail.com>
2013-11-10 23:51:20 +0900

    interface commit
    인터페이스관련한 소스코딩입니다.
    
    일부 reflcetion과 관련한소스도 포합니다.

---
+++ interface_study/src/TestImpl2.java
@@ -0,0 +1,15 @@
+
+public  class TestImpl2 extends TestAstract{
+
+	@Override
+	public void doA() throws Exception {
+		System.out.println("货肺款 矫胶袍");
+		
+	}
+
+	@Override
+	public void doB() throws Exception {
+		System.out.println("客快");
+	}
+	
+}
